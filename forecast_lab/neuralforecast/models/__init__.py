bb8237c038548fed0617da1a4bcf8899e0dc0a0b
wangzhefeng <wangzhefengr@163.com>
2025-01-16 23:13:03 +0800

    update

---
+++ forecast_lab/neuralforecast/models/__init__.py
@@ -0,0 +1,40 @@
+__all__ = ['RNN', 'GRU', 'LSTM', 'TCN', 'DeepAR', 'DilatedRNN',
+           'MLP', 'NHITS', 'NBEATS', 'NBEATSx', 'DLinear', 'NLinear',
+           'TFT', 'VanillaTransformer', 'Informer', 'Autoformer', 'PatchTST', 'FEDformer',
+           'StemGNN', 'HINT', 'TimesNet', 'TimeLLM', 'TSMixer', 'TSMixerx', 'MLPMultivariate',
+           'iTransformer', 'BiTCN', 'TiDE', 'DeepNPTS', 'SOFTS', 'TimeMixer', 'KAN', 'RMoK',
+           ]
+
+from .rnn import RNN
+from .gru import GRU
+from .lstm import LSTM
+from .tcn import TCN
+from .deepar import DeepAR
+from .dilated_rnn import DilatedRNN
+from .mlp import MLP
+from .nhits import NHITS
+from .nbeats import NBEATS
+from .nbeatsx import NBEATSx
+from .dlinear import DLinear
+from .nlinear import NLinear
+from .tft import TFT
+from .stemgnn import StemGNN
+from .vanillatransformer import VanillaTransformer
+from .informer import Informer
+from .autoformer import Autoformer
+from .fedformer import FEDformer
+from .patchtst import PatchTST
+from .hint import HINT
+from .timesnet import TimesNet
+from .timellm import TimeLLM
+from .tsmixer import TSMixer
+from .tsmixerx import TSMixerx
+from .mlpmultivariate import MLPMultivariate
+from .itransformer import iTransformer
+from .bitcn import BiTCN
+from .tide import TiDE
+from .deepnpts import DeepNPTS
+from .softs import SOFTS
+from .timemixer import TimeMixer
+from .kan import KAN
+from .rmok import RMoK
