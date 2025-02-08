309f3dccb0756f36629e1086c808e53bb53b9a33
wangzhefeng <wangzhefengr@163.com>
2025-02-08 18:39:44 +0800

    update data_provider

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -18,6 +18,7 @@ ROOT = os.getcwd()
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 
+import torch
 from torch.utils.data import DataLoader
 
 from data_provider.data_loader import (
@@ -27,8 +28,15 @@ from data_provider.data_loader import (
     SMAPSegLoader, SMDSegLoader,
     SWATSegLoader, UEAloader
 )
+from data_provider.data_loader import (
+    Dataset_Train,
+    Dataset_Test,
+    Dataset_Pred,
+)
 from data_provider.uea import collate_fn
 from data_provider.data_loader import Data_Loader
+
+from utils.log_util import logger
 
 # global variable
 LOGGING_LABEL = __file__.split('/')[-1][:-3]
@@ -138,6 +146,68 @@ def data_provider(args, flag: str):
         return data_set, data_loader
 
 
+def data_provider_new(args, flag, pre_data):
+    """
+    数据集构造
+
+    Args:
+        args (_type_): 参数
+        flag (_type_): 任务标签
+        pre_data (_type_): TODO
+
+    Returns:
+        _type_: _description_
+    """
+    # 是否对时间戳进行编码
+    timeenc = 0 if args.embed != 'timeF' else 1
+    # 区别在 test 和 train/valid 任务下是否进行 shuffle 数据
+    shuffle_flag = False# if (flag == 'test' or flag == 'TEST') else True
+    # 是否丢弃最后一个 batch
+    drop_last = True
+    # 数据集参数
+    if flag == "train":
+        batch_size = args.batch_size
+        Data = Dataset_Train
+    elif flag == 'val':
+        batch_size = args.batch_size
+        Data = Dataset_Train
+    elif flag == 'test':
+        batch_size = args.batch_size
+        Data = Dataset_Test
+    elif flag == 'pred':
+        batch_size = 1
+        pre_data = pre_data
+        Data = Dataset_Pred
+    
+    # 构建 Dataset 和 DataLoader
+    # Dataset
+    data_set = Data(
+        root_path = args.root_path,
+        data_path = args.data_path,
+        pre_data = pre_data,
+        flag = flag,
+        size = [args.seq_len, args.label_len, args.pred_len],
+        features = args.features,
+        target = args.target,
+        scale = args.scale,
+        timeenc = timeenc,
+        freq = args.freq,
+        inverse = args.inverse,
+        cols = None,
+    )
+    logger.info(f"Task: {flag}, data_set len: {len(data_set)}")
+    # DataLoader
+    data_loader = DataLoader(
+        data_set,
+        batch_size = batch_size,
+        shuffle = shuffle_flag,
+        num_workers = args.num_workers,
+        drop_last = drop_last,
+    )
+    
+    return data_set, data_loader
+
+
 
 
 # 测试代码 main 函数
@@ -214,6 +284,84 @@ def main():
         dec_inp = torch.cat([batch_y[:, :args.label_len, :], dec_inp], dim = 1).float()
         print(dec_inp)
         break
+    # ------------------------------
+    # data_provider_new test
+    # ------------------------------
+    from scripts.aidc_load_forecast.get_args import get_args_script_ETTh
+    from exp.exp_forecast_dl import Exp_Forecast
+    # params
+    args = get_args_script_ETTh()
+    
+    # 实例化模型
+    exp = Exp_Forecast(args)
+    
+    # data
+    train_data, train_loader = exp._get_data(flag='pred', pre_data=None)
+    
+    train_steps = len(train_loader)
+    logger.info(f"train_steps: {train_steps}")
+    
+    for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
+        # 当前 epoch 的迭代次数记录
+        # iter_count += 1
+        
+        # 模型优化器梯度归零
+        # model_optim.zero_grad()
+        
+        # 数据预处理
+        batch_x = batch_x.float().to(exp.device)
+        batch_y = batch_y.float().to(exp.device)
+        logger.info(f"{i}, batch_x.shape: {batch_x.shape}, batch_y.shape: {batch_y.shape}")
+        logger.info(f"{i}, batch_x: \n{batch_x}")
+        logger.info(f"{i}, batch_y: \n{batch_y}")
+        
+        batch_x_mark = batch_x_mark.float().to(exp.device)
+        batch_y_mark = batch_y_mark.float().to(exp.device)
+        logger.info(f"{i}, batch_x_mark.shape: {batch_x_mark.shape}, batch_y_mark.shape: {batch_y_mark.shape}")
+        logger.info(f"{i}, batch_x_mark: \n{batch_x_mark}")
+        logger.info(f"{i}, batch_y_mark: \n{batch_y_mark}")
+        # ------------------------------
+        # 前向传播
+        # ------------------------------
+        # decoder input
+        dec_inp = torch.zeros_like(batch_y[:, -exp.args.pred_len:, :]).float()
+        logger.info(f"{i}, dec_inp.shape: {dec_inp.shape}")
+        logger.info(f"{i}, dec_inp: \n{dec_inp}")
+        
+        dec_inp = torch.cat([batch_y[:, :exp.args.label_len, :], dec_inp], dim=1).float().to(exp.device)
+        logger.info(f"{i}, dec_inp.shape: {dec_inp.shape}")
+        logger.info(f"{i}, dec_inp: \n{dec_inp}")
+        
+        # encoder-decoder
+        # outputs = exp.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+        # logger.info(f"{i}, outputs.shape: {outputs.shape}")
+        # logger.info(f"{i}, outputs: \n{outputs}")
+        
+        # ------------------------------
+        # DataEmbedding
+        # ------------------------------
+        from layers.Embed import TokenEmbedding, PositionalEmbedding, TemporalEmbedding, TimeFeatureEmbedding
+
+        token_embed = TokenEmbedding(c_in=7, d_model=64)
+        x = token_embed(batch_x)
+        print(x)
+        print(x.shape)
+        
+        pos_embed = PositionalEmbedding(d_model=64)
+        x = pos_embed(batch_x)
+        print(x)
+        print(x.shape)
+        
+        temporal_embed = TemporalEmbedding(
+            d_model=64, embed_type=args.embed, freq=args.freq,
+        ) if args.embed != 'timeF' else TimeFeatureEmbedding(
+            d_model=64, embed_type=args.embed, freq=args.freq,
+        )
+        x = temporal_embed(batch_x_mark)
+        print(x)
+        print(x.shape)
+        
+        if i == 0: break
 
 if __name__ == "__main__":
     main()
--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -20,7 +20,375 @@ from data_provider.uea import Normalizer, interpolate_missing, subsample
 from utils.augmentation import run_augmentation_single
 from utils.timefeatures import time_features
 
+from utils.log_util import logger
+
 warnings.filterwarnings('ignore')
+
+
+class Dataset_Train(Dataset):
+    
+    def __init__(self, 
+                 root_path, 
+                 data_path,
+                 pre_data=None,
+                 flag='train', 
+                 size=None,  # size [seq_len, label_len, pred_len]
+                 features='S', 
+                 target='OT',
+                 scale=True,
+                 timeenc=0,
+                 freq='h',
+                 inverse = True,
+                 cols = None):
+        self.root_path = root_path
+        self.data_path = data_path
+        assert flag in ['train', 'test', 'val']
+        type_map = {'train': 0, 'val': 1, 'test': 2}
+        self.set_type = type_map[flag]
+        self.seq_len = 24 * 4 * 4 if size is None else size[0]
+        self.label_len = 24 * 4 if size is None else size[1]
+        self.pred_len = 24 * 4 if size is None else size[2]
+        self.features = features
+        self.target = target
+        self.freq = freq
+        self.scale = scale
+        self.inverse = inverse
+        self.timeenc = timeenc
+        # 读取数据
+        self.__read_data__()
+
+    def __read_data__(self):
+        # 数据文件(CSV)
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+        df_raw.dropna(axis=1, how='any', inplace=True)
+        # 数据特征排序
+        cols = list(df_raw.columns)
+        cols.remove(self.target)
+        cols.remove('date')
+        df_raw = df_raw[['date'] + cols + [self.target]]
+        # 预测特征变量数据
+        if self.features == 'M' or self.features == 'MS':
+            df_data = df_raw[df_raw.columns[1:]]
+        elif self.features == 'S':
+            df_data = df_raw[[self.target]]
+        # 训练/测试/验证数据集分割
+        # 数据分割比例
+        true_train = int(len(df_raw))                  # 1.0
+        num_train = int(len(df_raw) * 0.7)             # 0.7
+        num_test = int(len(df_raw) * 0.0)              # 0.0
+        num_vali = len(df_raw) - num_train - num_test  # 0.3
+        # 数据分割索引
+        border1s = [0,          num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]
+        # border2s = [num_train, num_train + num_vali,     len(df_raw)]
+        border2s = [true_train, num_train + num_vali,     len(df_raw)]
+        border1, border2 = border1s[self.set_type], border2s[self.set_type]
+        # 数据标准化
+        self.scaler = StandardScaler()
+        if self.scale:
+            train_data = df_data[border1s[0]:border2s[0]]
+            self.scaler.fit(train_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+        # 时间特征处理
+        df_stamp = df_raw[['date']][border1:border2]
+        df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        if self.timeenc == 0:
+            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
+            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
+            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
+            data_stamp = df_stamp.drop(['date'], axis=1).values
+        elif self.timeenc == 1:
+            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
+            data_stamp = data_stamp.transpose(1, 0)
+        # 数据切分
+        self.data_x = data[border1:border2]
+        self.data_y = data[border1:border2]
+        # if self.inverse:
+        #     self.data_y = df_data.values[border1:border2]
+        # else:
+        #     self.data_y = data[border1:border2]
+        self.data_stamp = data_stamp
+
+    def __getitem__(self, index):
+        # data_x 索引
+        # s_begin = index
+        s_begin = index * self.pred_len
+        s_end = s_begin + self.seq_len
+        # data_y 索引
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+        # 数据索引分割
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_end]
+        # if self.inverse:
+        #     seq_y = np.concatenate(
+        #         [self.data_x[r_begin:r_begin + self.label_len], self.data_y[r_begin + self.label_len:r_end]], 
+        #         axis = 0
+        #     )
+        # else:
+        #     seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len - self.pred_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
+
+
+class Dataset_Test(Dataset):
+    
+    def __init__(self, 
+                 root_path, 
+                 data_path,
+                 pre_data=None,
+                 flag='train', 
+                 size=None,  # size [seq_len, label_len, pred_len]
+                 features='S', 
+                 target='OT',
+                 scale=True,
+                 timeenc=0,
+                 freq='h',
+                 inverse = True,
+                 cols = None):
+        self.root_path = root_path
+        self.data_path = data_path
+        assert flag in ['test']
+        type_map = {'test': 0}
+        self.set_type = type_map[flag]
+        self.seq_len = 24 * 4 * 4 if size is None else size[0]
+        self.label_len = 24 * 4 if size is None else size[1]
+        self.pred_len = 24 * 4 if size is None else size[2]
+        self.features = features
+        self.target = target
+        self.freq = freq
+        self.scale = scale
+        self.inverse = inverse
+        self.timeenc = timeenc
+        # 读取数据
+        self.__read_data__()
+
+    def __read_data__(self):
+        # 数据文件(CSV)
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+        df_raw.dropna(axis=1, how='any', inplace=True)
+        # 数据特征排序
+        cols = list(df_raw.columns)
+        cols.remove(self.target)
+        cols.remove('date')
+        df_raw = df_raw[['date'] + cols + [self.target]]
+        # 预测特征变量数据
+        if self.features == 'M' or self.features == 'MS':
+            df_data = df_raw[df_raw.columns[1:]]
+        elif self.features == 'S':
+            df_data = df_raw[[self.target]]
+        # 训练/测试/验证数据集分割
+        # 数据分割比例
+        true_train = int(len(df_raw))                  # 1.0
+        num_train = int(len(df_raw) * 1.0)             # 1.0
+        num_test = int(len(df_raw) * 0.0)              # 0.0
+        num_vali = len(df_raw) - num_train - num_test  # 0.0
+        # 数据分割索引
+        border1s = [0,          num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]
+        # border2s = [num_train, num_train + num_vali,     len(df_raw)]
+        border2s = [true_train, num_train + num_vali,     len(df_raw)]
+        border1, border2 = border1s[self.set_type], border2s[self.set_type]
+        # 数据标准化
+        self.scaler = StandardScaler()
+        if self.scale:
+            train_data = df_data[border1s[0]:border2s[0]]
+            self.scaler.fit(train_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+        # 时间特征处理
+        df_stamp = df_raw[['date']][border1:border2]
+        df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        if self.timeenc == 0:
+            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
+            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
+            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
+            data_stamp = df_stamp.drop(['date'], axis=1).values
+        elif self.timeenc == 1:
+            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
+            data_stamp = data_stamp.transpose(1, 0)
+        # 数据切分
+        self.data_x = data[border1:border2]
+        self.data_y = data[border1:border2]
+        # if self.inverse:
+        #     self.data_y = df_data.values[border1:border2]
+        # else:
+        #     self.data_y = data[border1:border2]
+        self.data_stamp = data_stamp
+
+    def __getitem__(self, index):
+        # data_x 索引
+        # s_begin = index
+        s_begin = index * self.pred_len
+        s_end = s_begin + self.seq_len
+        # logger.info(f"s_begin: {s_begin}")
+        # logger.info(f"s_end: {s_end}")
+        # data_y 索引
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+        # logger.info(f"r_begin: {r_begin}")
+        # logger.info(f"r_end: {r_end}")
+        # 数据索引分割
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_end]
+        # if self.inverse:
+        #     seq_y = np.concatenate(
+        #         [self.data_x[r_begin:r_begin + self.label_len], self.data_y[r_begin + self.label_len:r_end]], 
+        #         axis = 0
+        #     )
+        # else:
+        #     seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len - self.pred_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
+
+
+class Dataset_Pred(Dataset):
+    
+    def __init__(self, 
+                 root_path, 
+                 data_path,
+                 pre_data=None, 
+                 flag='pred', 
+                 size=None,  # size: [seq_len, label_len, pred_len]
+                 features='S',
+                 target='OT', 
+                 scale=True, 
+                 timeenc=0, 
+                 freq='15min',
+                 inverse=True,
+                 cols=None):
+        # data file path
+        self.root_path = root_path
+        self.data_path = data_path
+        self.pre_data = pre_data
+        # data type
+        assert flag in ['pred']
+        # data size
+        self.seq_len = 24 * 4 * 4 if size is None else size[0]
+        self.label_len = 24 * 4 if size is None else size[1]
+        self.pred_len = 24 * 4 if size is None else size[2]
+        # data freq, feature columns, and target
+        self.freq = freq
+        self.features = features
+        self.cols = cols
+        self.timeenc = timeenc
+        self.target = target
+        # data preprocess
+        self.scale = scale
+        self.inverse = inverse
+        # data read
+        self.__read_data__()
+
+    def __read_data__(self):
+        # 数据文件(CSV)
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+        df_raw.dropna(axis=1, how='any', inplace=True)
+        # 因为进行预测的时候代码会自动帮我们取最后的数据我们只需做好拼接即可
+        if self.pre_data is not None and not self.pre_data.empty:
+            df_raw = pd.concat([df_raw, self.pre_data], axis=0, ignore_index=True)
+        # 数据特征排序
+        if self.cols:
+            cols = self.cols.copy()
+            cols.remove(self.target)
+        else:
+            cols = list(df_raw.columns)
+            cols.remove(self.target)
+            cols.remove('date')
+        df_raw = df_raw[['date'] + cols + [self.target]]
+        # 预测特征变量数据
+        if self.features == 'M' or self.features == 'MS':
+            df_data = df_raw[df_raw.columns[1:]]
+        elif self.features == 'S':
+            df_data = df_raw[[self.target]]
+        # 数据窗口索引
+        border1 = len(df_raw) - self.seq_len
+        border2 = len(df_raw)
+        # logger.info(f"border1: {border1}")
+        # logger.info(f"border2: {border2}")
+        # 数据标准化
+        self.scaler = StandardScaler()
+        if self.scale:
+            self.scaler.fit(df_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+        # logger.info(f"data: \n{data}")
+        # logger.info(f"data.shape: {data.shape}")
+        # 时间戳特征处理
+        tmp_stamp = df_raw[['date']][border1:border2]
+        tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date, format='mixed')
+        pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len + 1, freq=self.freq)
+        
+        df_stamp = pd.DataFrame(columns=['date'])
+        df_stamp.date = list(tmp_stamp.date.values) + list(pred_dates[1:])
+        if self.timeenc == 0:
+            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
+            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
+            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
+            df_stamp['minute'] = df_stamp.date.apply(lambda row: row.minute, 1)
+            df_stamp['minute'] = df_stamp.minute.map(lambda x: x // 15)
+            data_stamp = df_stamp.drop(['date'], axis=1).values
+        elif self.timeenc == 1:
+            data_stamp = time_features(
+                pd.to_datetime(df_stamp['date'].values), 
+                freq=self.freq
+            )
+            data_stamp = data_stamp.transpose(1, 0)
+        # 数据切分
+        self.data_x = data[border1:border2]
+        if self.inverse:
+            self.data_y = df_data.values[border1:border2]
+        else:
+            self.data_y = data[border1:border2]
+        self.data_stamp = data_stamp
+        # logger.info(f"self.data_x: \n{self.data_x}")
+        # logger.info(f"self.data_x.shape: {self.data_x.shape}")
+        # logger.info(f"self.data_y: \n{self.data_y}")
+        # logger.info(f"self.data_y.shape: {self.data_y.shape}")
+
+    def __getitem__(self, index):
+        # data_x 索引
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        # data_y 索引
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+        # 数据索引分割
+        seq_x = self.data_x[s_begin:s_end]
+        if self.inverse:
+            seq_y = self.data_x[r_begin:r_begin + self.label_len]
+        else:
+            seq_y = self.data_y[r_begin:r_begin + self.label_len]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data.detach().cpu().numpy())
 
 
 class Dataset_Custom(Dataset):
